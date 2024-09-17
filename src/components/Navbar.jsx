f084dbe00cbd3012dd917958804d309f0e958f75
pranavsoni27 <pranavsoni2702@gmail.com>
2024-09-17 16:51:34 +0530

    final hai

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = () => {
 
                 <div className={` ${showMenu ? 'fixed w-full' : 'h-0 w-0'}  md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
                         <div className='flex items-center justify-between px-5 py-6'>
-                                <img className='w-36' src={assets.logo} alt="" />
+                                <img className='w-36' src={assets.qdm} alt="" />
                                 <img className='w-7' onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="" />
                         </div>
                         <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
